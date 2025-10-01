e5fdd8e4c208d88b7022f3e60a99e8a70b8adcf7
ransakhu <ransakhu@gmail.com>
2025-10-01 15:32:42 +0300

    food tracker header

---
+++ BloomCycle/ContentView 2.swift
@@ -0,0 +1,122 @@
+//
+//  ContentView 2.swift
+//  BloomCycle
+//
+//  Created by rand on 02/10/2025.
+//
+
+
+import SwiftUI
+
+struct FoodTracker: View {
+    let formatter: DateFormatter = {
+        let yf = DateFormatter()
+        yf.dateFormat = "yyyy"   // سنة
+        return yf
+    }() 
+
+    let Mformatter: DateFormatter = {
+        let mf = DateFormatter()
+        mf.dateFormat = "MMMM"   // شهر كامل
+        return mf
+    }()
+
+    var body: some View {
+        VStack(spacing: 0) {
+      VStack {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 9) {
+                        Button(action: {}) {
+                            HStack(spacing: 4) {
+                                Image(systemName: "chevron.left")
+                                Text("Back")
+                            }
+                            .font(.headline)
+                            .foregroundColor(.br)
+                        }
+
+                        HStack(spacing: 4) {
+                            
+                            Text(Mformatter.string(from: Date()))
+                                .font(.largeTitle).fontWeight(.bold)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.8)
+                                .layoutPriority(1)
+                                .foregroundColor(.dbrown)
+                            
+                            Text(formatter.string(from: Date()))
+                                .font(.largeTitle).fontWeight(.bold)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.8)
+                                .layoutPriority(1)
+                                .foregroundColor(.dpink)
+
+                        }
+                    }
+
+                    Spacer()
+
+                    Button {} label: {
+                        Image(systemName: "calendar")
+                            .font(.system(size: 28, weight: .semibold))
+                            .foregroundColor(.dbrown)
+                            .padding(8)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color(.lpink))
+                            )
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top, 8)
+
+                Spacer(minLength: 30)
+            }
+
+            Spacer()
+
+            // المستطيل الابيض
+            ZStack {
+                RoundedCorner(radius: 30, corners: [.topLeft, .topRight])
+                    .fill(Color.white)
+                    .frame(maxWidth: .infinity)
+                    .ignoresSafeArea(edges: .bottom)
+                    .frame(height: 600)
+                    
+
+                Text("No meals yet, \nAdd your meal!")
+                    .font(.system(size: 26, weight: .bold))
+                    .foregroundColor(.lbrown)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        // الخلفية
+        .background(
+            LinearGradient(
+                gradient: Gradient(colors: [Color.lyellow.opacity(0.8), Color.dpink.opacity(0.9)]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+        )
+    }
+}
+
+// for the rec to
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        return Path(path.cgPath)
+    }
+}
+
+#Preview {
+    FoodTracker()
+}
--- BloomCycle/ContentView.swift
+++ BloomCycle/ContentView.swift
@@ -4,18 +4,101 @@
 //
 //  Created by Sarah on 03/04/1447 AH.
 //
-
 import SwiftUI
 
 struct ContentView: View {
- 
+    @State private var scrollOffset: CGFloat = 0
     var body: some View {
+        
+        
+        ZStack{
+            Color.color
+                .ignoresSafeArea()
+            Image("background")
+                            .resizable()
+                            .ignoresSafeArea()
        
-       
+            VStack{
+                Image("blood")
+                    .resizable()
+                    .frame(width: 95, height: 110)
+                    .padding(5)
+
+                Text("Menstrual Phase")
+                    .font(.system(size: 30, weight: .bold, design: .default))
+                    .foregroundColor(.br)
+                
+                
+                Spacer()
+                
+                
+            }
+            
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                               HStack(spacing: 20) {
+                                   CardView(title: "Needs", color: .greenn, borderColor: .br, imageName: "NEED",subTitle: " Replace blood loss (iron + vitamin C)")
+                                   CardView(title: "Foods", color: .greenn, borderColor: .br, imageName: "FOOD",subTitle: " Spinach, lentils, red meat, liver, oranges, strawberries")
+                                  
+                               }
+                
+                               .padding(.horizontal, 20)
+                
+                           }
+                           .frame(height: 300)
+                           .padding(.top, 60)
+        
+                       .padding()
+        }
     }
-    
+
 }
 
+
+
+
+struct CardView: View {
+    var title: String
+    var color: Color
+    var borderColor: Color
+    var imageName: String  // اسم الصورة
+    var subTitle: String
+    
+    var body: some View {
+        VStack(spacing: 10) {  // يعطي مسافة بين الصورة والنصوص
+ 
+            Image(imageName)
+                .resizable()
+                    .scaledToFill()   // يملأ الإطار كاملًا
+                    .frame(width: 250, height: 150)  // حدد حجم الكارد أو الجزء اللي تبيه
+                    .clipped()        // يقص أي جزء زايد خارج الإطار
+                    .cornerRadius(500) // إذا تحب تكون الزوايا
+            
+            VStack(spacing: 5) { // مجموعة النصوص
+                Text(title)
+                    .font(.system(size: 30, weight: .bold))  // حجم أصغر
+                    .foregroundColor(borderColor)
+                    .multilineTextAlignment(.center) // توسيط النص
+                
+                Text(subTitle)
+                    .font(.system(size: 24))  // حجم مناسب للوصف
+                    .foregroundColor(borderColor)
+                    .multilineTextAlignment(.center) // توسيط النص
+            }
+            .padding(.horizontal, 10) // padding من الجوانب
+
+        }
+        .frame(width: 300, height: 350)
+        .background(color) // لون خلفية الكارد
+        .cornerRadius(16)
+        .overlay(
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(borderColor, lineWidth: 3) // البورد
+        )
+        .shadow(radius: 5)
+        .padding(.vertical, 20)
+    }
+}
 
 #Preview {
     ContentView()
