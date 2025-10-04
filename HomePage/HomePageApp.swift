9739ce0a24d11e325303a4a874b640cf9037f39d
SarahAlnasser <sarahalnasser04@gmail.com>
2025-10-04 21:09:45 +0300

    Initial Commit

---
+++ HomePage/HomePageApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomePageApp.swift
+//  HomePage
+//
+//  Created by Sarah Alnasser on 04/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct HomePageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
